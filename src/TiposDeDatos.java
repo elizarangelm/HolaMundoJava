53d6ba6fe5c5fd348f7bbfa257be7a49150bb280
Elizabeth <elizarangelm24@gmail.com>
2021-10-21 11:31:07 -0500

    Repasando conceptos Java

---
+++ src/TiposDeDatos.java
@@ -0,0 +1,17 @@
+
+public class TiposDeDatos {
+      public static void main(String args[]){
+          //Definir variable
+          int miVariableEntera = 10;
+          System.out.println(miVariableEntera);
+          //Modificamos el valor de la variable
+          miVariableEntera = 5;
+          System.out.println(miVariableEntera);
+          
+          String miVariableCadena = "Saludos";
+          System.out.println(miVariableCadena);
+          
+          miVariableCadena = "Adios";
+          System.out.println(miVariableCadena);
+      }   
+}
