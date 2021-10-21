9524a2469bac0628336c9a8497f6679efa9cf0a4
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2021-10-21 18:25:41 -0500

    Caracteres especiales y utilidad Scanner

--- src/Libro.java
+++ src/Libro.java
@@ -1,14 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Elizabeth Rangel M
- */
 public class Libro {
 
     public static void main(String args[]) {
