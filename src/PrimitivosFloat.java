0d9a1fa70310f05b80ee6db586c1b212e7499320
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2021-10-21 19:00:14 -0500

    Primitivos flotantes

--- src/PrimitivosFloat.java
+++ src/PrimitivosFloat.java
@@ -8,20 +8,21 @@
  * @author Elizabeth Rangel M
  */
 public class PrimitivosFloat {
+
     public static void main(String[] args) {
         // primitivos float: float y double
-        float numFloat1 = (float)10.0;
+        float numFloat1 = (float) 10.0;
         float numFloat2 = 10.0F;
-        
-        float numFloat = 3.4028235E38F; 
+
+        float numFloat = 3.4028235E38F;
         System.out.println("numFloat = " + numFloat);
         System.out.println("valor minimo Float: " + Float.MIN_VALUE); //1.4E-45
         System.out.println("valor maximo Float: " + Float.MAX_VALUE); //3.4028235E38
-        
+
         double numDouble = 1.7976931348623157E308;
         System.out.println("numDouble = " + numFloat);
         System.out.println("valor minimo Double: " + Double.MIN_VALUE); //4.9E-324
         System.out.println("valor maximo Double: " + Double.MAX_VALUE); //1.7976931348623157E308
-        
+
     }
 }
