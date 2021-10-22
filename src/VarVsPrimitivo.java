1c1c1555d8a049dc19b6e345227783206c635be3
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2021-10-21 19:12:04 -0500

    Var vs Primitivo

---
+++ src/VarVsPrimitivo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elizabeth Rangel M
+ */
+public class VarVsPrimitivo {
+    public static void main(String[] args) {
+        
+        /* Para ejecutar este proyecto y ver como se asignan el tipo de dato con
+        var se puede pausar la linea, ejecutar debug, y ejecutar linea por linea
+        con F8, en la ventana de variables podemos ver esta informacion
+        */
+        var numeroEntero = 10;  
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroDouble = 10.0;  
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;  
+        System.out.println("numeroFloat = " + numeroFloat);
+               
+    }
+}
