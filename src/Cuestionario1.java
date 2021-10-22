c60e2dadb15162298dd53d979902cbd4c16dfdd8
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2021-10-21 20:08:24 -0500

    Cuestionario

---
+++ src/Cuestionario1.java
@@ -0,0 +1,22 @@
+
+public class Cuestionario1 {
+
+    public static void main(String[] args) {
+//        var nombre = "Juan";
+//        System.out.println(nombre);
+//        nombre = "Carlos";
+//        System.out.println(nombre);
+//
+//        String color;
+//        System.out.println(color);// (la variable color tal vez no ha sido inicializada)
+//        color = "Rojo";
+
+//        a = "A";
+//        System.out.println(a);
+//        a = "B";
+//        System.out.println(b);
+//        // La variable a no se ha definido, no tiene un tipo definido.
+        var a = 5.3; 
+        System.out.println("a = " + a);
+    }
+}
