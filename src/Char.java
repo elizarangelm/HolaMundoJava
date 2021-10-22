ab2fd5bc8ea2c4ce9e7d827fa0e3b8ff4deda02f
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2021-10-21 19:26:11 -0500

    Tipo Char

--- src/Char.java
+++ src/Char.java
@@ -1,5 +1,6 @@
-public class Char {
 
+public class Char {
+    
     public static void main(String[] args) {
         char miCaracter = 'a';
         System.out.println("miCaracter = " + miCaracter);
@@ -7,29 +8,26 @@ public class Char {
         //lista unicode latam
         char varChar = '\u0021';
         System.out.println("varChar = " + varChar);
-
+        
         char varCharDecimal = 33;
         System.out.println("varCharDecimal = " + varCharDecimal);
         
         char varCharSimbolos = '!';
         System.out.println("varCharSimbolos = " + varCharSimbolos);
-        
+
         //haciendo lo mismo con var 
-         
         var varChar2 = '\u0021';
         System.out.println("varChar2 = " + varChar2);
-
+        
         var varCharDecimal2 = 33; //se guarda tipo entero
         System.out.println("varCharDecimal2 = " + varCharDecimal2);
         
         var varCharSimbolos2 = '!';
         System.out.println("varCharSimbolos2 = " + varCharSimbolos2);
-        
+
         //si ser imprime como entero devolvera su valor en decimal
-        
         int varEnteraSimbolos = '!';
         System.out.println("varEnteraSimbolos = " + varEnteraSimbolos);
-
-
+        
     }
 }
